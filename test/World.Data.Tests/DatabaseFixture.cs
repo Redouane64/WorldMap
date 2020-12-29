09aea711e8e01f4edfb69fd8684a825f59c99b70
Redhouane Sobaihi <redouane.sobaihi@live.com>
2020-12-29 21:40:26 +0700

    add database connection setup

---
+++ test/World.Data.Tests/DatabaseFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Data.Sqlite;
+
+using Xunit;
+
+namespace World.Data.Tests
+{
+	public class DatabaseFixture : IDisposable
+	{
+		public DatabaseFixture()
+		{
+			this.Db = new SqliteConnection("Data Source=world.db");
+
+		}
+
+		public void Dispose() => this.Db.Dispose();
+
+		public IDbConnection Db { get; private set; }
+	}
+}
